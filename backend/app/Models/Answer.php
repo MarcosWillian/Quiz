b4cd7575673d3f73d454ca87a7c2b6195f8821be
= <--get>
2022-03-01 22:36:42 -0300

    add hidden parameters in Answer model

--- backend/app/Models/Answer.php
+++ backend/app/Models/Answer.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use App\Traits\PrimaryKeyUuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Answer extends Model
 {
     use PrimaryKeyUuid;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -20,4 +22,12 @@ class Answer extends Model
         'question_id'
     ];
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'correct'
+    ];
 }
