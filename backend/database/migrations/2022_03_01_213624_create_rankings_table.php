4293d481a55a35768d763900233ee44053a1b9c6
= <--get>
2022-03-01 22:29:51 -0300

    add migration to rankings table

---
+++ backend/database/migrations/2022_03_01_213624_create_rankings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRankingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rankings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('user', 200);
+            $table->integer('total_points');
+            $table->integer('total_answers');
+            $table->integer('position');
+            $table->timestamps();
+            $table->softDeletes();	
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rankings');
+    }
+}
