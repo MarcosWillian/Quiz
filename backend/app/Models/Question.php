67141c85ddad022d473ac920cef6ec6aee07bee0
= <--get>
2022-03-01 22:35:54 -0300

    add relationship answers in Question model

--- backend/app/Models/Question.php
+++ backend/app/Models/Question.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use App\Traits\PrimaryKeyUuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
     use PrimaryKeyUuid;
+    use SoftDeletes;
     
     /**
      * The attributes that are mass assignable.
@@ -19,4 +21,12 @@ class Question extends Model
         'observation',
         'points'
     ];
+
+    /**
+     * Get the answers for the question.
+     */
+    public function answers()
+    {
+        return $this->hasMany('App\Models\Answer');
+    }
 }
